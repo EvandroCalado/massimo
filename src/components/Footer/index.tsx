b256c7e0c33245fe6e3b295f2eac5b3b73aaaa8c
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-10-09 10:40:52 -0300

    :style: fix style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <div className="flex h-12 items-center justify-between p-4 text-red-500 md:h-24 lg:p-20 xl:p-40">
+    <div className="flex h-12 items-center justify-between border-t-2 border-red-500 p-4 text-red-500 md:h-24 lg:px-20 xl:px-40">
       <Link href={'/'} className="text-xl font-bold uppercase">
         Massimo
       </Link>
