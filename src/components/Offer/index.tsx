a5fb252890501e4624d98b78a1c3b70958875389
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-10-10 23:15:39 -0300

    :styles: change styles

--- src/components/Offer/index.tsx
+++ src/components/Offer/index.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
+import Button from '../Button';
 import CountDown from '../CountDown';
 
 export default function Offer() {
   return (
-    <div className="flex h-screen flex-col bg-black p-4 md:flex-row md:justify-between">
+    <div className="flex h-screen flex-col bg-black p-4 md:h-[70vh] md:flex-row md:justify-between">
       {/* TEXT CONTAINER */}
-      <div className="flex h-[50vw] flex-1 flex-col items-center justify-center gap-4 p-6 text-center md:h-full md:gap-8">
+      <div className="flex h-[50vw] flex-1 flex-col items-center justify-center gap-4 p-6 text-center md:h-full md:gap-6 lg:gap-8">
         <h1 className="text-3xl font-bold text-white md:text-5xl xl:text-6xl">
           O hambúrguer que vai te surpreender
         </h1>
@@ -15,9 +16,7 @@ export default function Offer() {
           água na boca.
         </p>
         <CountDown />
-        <button className="bg-red-500 px-4 py-2 text-white duration-150 hover:bg-red-600 md:px-8 md:py-4">
-          Peça já
-        </button>
+        <Button>Peça já</Button>
       </div>
       {/* IMAGE CONTAINER */}
       <div className="relative m-4 h-[50vw] w-full flex-1 md:h-full">
