454b32149f5f905ad5eecd7b1290d288fecfa46b
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-10-14 11:34:45 -0300

    :feat: add toggle and user links components

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import Cart from '../Cart';
 import Menu from '../Menu';
+import Toggle from '../Toggle';
+import { UserLinks } from '../UserLinks';
 
 export default function Navbar() {
-  const user = false;
-
   return (
     <div className="flex h-12 items-center justify-between border-b-2 border-b-red-500 p-4 uppercase text-red-500 md:h-24 lg:px-20 xl:px-40">
       {/* LEFT LINKS */}
@@ -24,15 +24,12 @@ export default function Navbar() {
       </div>
       {/* RIGHT LINKS */}
       <div className="hidden flex-1 items-center justify-end gap-4 md:flex">
+        <Toggle />
         <div className="flex cursor-pointer items-center gap-2 rounded-md bg-orange-300 px-2 py-1 text-[10.5px] duration-150 hover:brightness-95 lg:text-base">
           <Image src="/icons/phone.png" alt="Phone" width={20} height={20} />
-          <span>(99) 99999-9999</span>
+          <span>(99)9999-9999</span>
         </div>
-        {!user ? (
-          <Link href={'/login'}>Entrar</Link>
-        ) : (
-          <Link href={'/orders'}>Pedidos</Link>
-        )}
+        <UserLinks />
         <Cart />
       </div>
     </div>
