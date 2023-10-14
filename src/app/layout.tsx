4b464ab451f8a82df7f0873475e2b22c8df376ca
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-10-14 11:33:14 -0300

    :feat: add auth provider and query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
+import AuthProvider from '../components/AuthProvider';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import Notification from '../components/Notification';
+import QueryProvider from '../components/QueryProvider';
 import '../styles/globals.css';
 
 const openSans = Open_Sans({ subsets: ['latin'], display: 'swap' });
@@ -19,12 +21,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={openSans.className}>
-        <Notification />
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
+      <AuthProvider>
+        <QueryProvider>
+          <body className={openSans.className}>
+            <Notification />
+            <Navbar />
+            <main>{children}</main>
+            <Footer />
+          </body>
+        </QueryProvider>
+      </AuthProvider>
     </html>
   );
 }
